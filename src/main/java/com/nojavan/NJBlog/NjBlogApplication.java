d102fcd2810b27ebe96e7d75b4b135247df21d5e
sajad <sadjadnojavan@gmail.com>
2023-04-16 21:29:00 +0430

    Initial Commit

---
+++ src/main/java/com/nojavan/NJBlog/NjBlogApplication.java
@@ -0,0 +1,13 @@
+package com.nojavan.NJBlog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NjBlogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NjBlogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nojavan/NJBlog/NjBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nojavan.NJBlog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NjBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
